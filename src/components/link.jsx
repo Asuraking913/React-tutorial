6c5216aaa89eb364e43b114795dd83efc0ae0fee
Asuraking913 <israelshedrack913@gmail.com>
2024-01-26 12:36:37 +0100

    new changes

--- src/components/link.jsx
+++ src/components/link.jsx
@@ -1,13 +1,13 @@
-export default function LinkTag(props) {
+export default function LinkTag({ icon, info }) {
   return (
     <div className="w-[80%] flex flex-col items-center justify-center ">
       <a
         href=""
         className="flex rounded-full hover:scale-110 duration-200 ease-in shadow-lg shadow-gray-500 gap-4 p-4 items-center justify-center text-2xl capitalize bg-gray-300 w-full"
       >
-        <p className="capitalize text-3xl">{props.icon}</p>
+        <p className="capitalize text-3xl">{icon}</p>
 
-        <p className="font-bold">{props.info}</p>
+        <p className="font-bold">{info}</p>
       </a>
     </div>
   );
