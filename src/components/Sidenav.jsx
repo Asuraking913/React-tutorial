cd4857eee057c1f7382d64b941bce955db0de5b7
Asuraking913 <israelshedrack913@gmail.com>
2024-01-26 12:24:47 +0100

    new changes

--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -1,14 +1,14 @@
 import React, { useState } from "react";
-// import {
-//   AiOutlineMenu,
-//   AiOutlineHome,
-//   AiOutlineProject,
-//   AiOutlineMail,
-// } from "react-icons/ai";
+import {
+  AiOutlineMenu,
+  AiOutlineHome,
+  AiOutlineProject,
+  AiOutlineMail,
+} from "react-icons/ai";
 import { BsPerson } from "react-icons/bs";
 import { GrProjects } from "react-icons/gr";
 import LinkTag from "./link";
-import { AiOutlineHome, AiOutlineMenu } from "react-icons/ai";
+// import { AiOutlineHome, AiOutlineMenu } from "react-icons/ai";
 
 const Sidenav = () => {
   const [nav, setNav] = useState(false);
@@ -17,12 +17,19 @@ const Sidenav = () => {
   };
 
   return (
-    <div className=" p-4  flex flex-row justify-start absolute bg-blue-500 w-full">
-      <AiOutlineMenu className="cursor-pointer text-2xl " onClick={handleNav} />
+    <div className=" p-4  flex flex-row justify-end absolute bg-blue-500 w-full">
+      <AiOutlineMenu
+        className="cursor-pointer text-2xl z-[99]"
+        onClick={handleNav}
+      />
 
       {nav ? (
-        <div className="flex h-screen w-full bg-white  fixed top-0 left-0 justify-center items-center">
+        <div className="flex flex-col gap-5 h-screen w-full bg-white  fixed top-0 left-0 justify-center items-center">
           <LinkTag info={"home"} icon={<AiOutlineHome />} />
+          <LinkTag info={"Work"} icon={<GrProjects />} />
+          <LinkTag info={"Projects"} icon={<AiOutlineProject />} />
+          <LinkTag info={"Resume"} icon={<BsPerson />} />
+          <LinkTag info={"Contact"} icon={<AiOutlineMail />} />
         </div>
       ) : (
         ""
--- src/components/link.jsx
+++ src/components/link.jsx
@@ -1,9 +1,9 @@
 export default function LinkTag(props) {
   return (
-    <div className="w-[80%] flex items-center justify-center ">
+    <div className="w-[80%] flex flex-col items-center justify-center ">
       <a
         href=""
-        className="flex rounded-full hover:scale-110 duration-200 ease-in shadow-lg shadow-gray-500 gap-4 h-[75px] items-center justify-center text-2xl capitalize bg-gray-300 w-full"
+        className="flex rounded-full hover:scale-110 duration-200 ease-in shadow-lg shadow-gray-500 gap-4 p-4 items-center justify-center text-2xl capitalize bg-gray-300 w-full"
       >
         <p className="capitalize text-3xl">{props.icon}</p>
 
