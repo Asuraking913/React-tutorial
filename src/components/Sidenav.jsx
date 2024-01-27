fe294f8b47a9d1886ef59404778fc31d3d6ed013
Asuraking913 <israelshedrack913@gmail.com>
2024-01-27 16:48:04 +0100

    new changes

--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -1,59 +1,56 @@
 import React, { useState } from "react";
-import {
-  AiOutlineMenu,
-  AiOutlineHome,
-  AiOutlineProject,
-  AiOutlineMail,
-} from "react-icons/ai";
-import { BsPerson } from "react-icons/bs";
-import { GrProjects } from "react-icons/gr";
-import LinkTag from "./link";
+// import {
+//   AiOutlineMenu,
+//   AiOutlineHome,
+//   AiOutlineProject,
+//   AiOutlineMail,
+// } from "react-icons/ai";
+// import { BsPerson } from "react-icons/bs";
+// import { GrProjects } from "react-icons/gr";
+// import LinkTag from "./link";
+import testTag from "./link";
+
 // import { AiOutlineHome, AiOutlineMenu } from "react-icons/ai";
 
-const Sidenav = () => {
-  const [nav, setNav] = useState(false);
-  const handleNav = () => {
-    setNav(!nav);
-  };
+function Sidenav() {
+  const jokes = [
+    {
+      joke: "Asdsdfsdfsfsffdfdsffs",
+      author: "Arnold Scgwharzengger",
+    },
 
-  const infoIcon2 = {
-    info: "projects",
-    icon: <AiOutlineProject />,
-  };
+    {
+      joke: "sdgjsaddsfgdfgdf",
+      author: "Asura King913",
+    },
+    {
+      joke: "sdasfwe3242342354",
+      author: "Ikki Tenrio",
+    },
+    {
+      joke: "sdfsfsfsdfgtyhk",
+      author: "Tang San",
+    },
+    {
+      joke: "sdffwpwpepjwer",
+      author: "Ling Luochen",
+    },
+  ];
 
-  const infoIcon = {
-    info: "home",
-    icon: <AiOutlineHome />,
-    info1: "work",
-    icon1: <GrProjects />,
-    info2: "projects",
-    icon2: <AiOutlineProject />,
-    info3: "resume",
-    icon3: <BsPerson />,
-    info4: "contact",
-    icon4: <AiOutlineMail />,
-  };
+  const jokeList = [
+    <testTag joke={jokes[0].joke} author={jokes[0].author} />,
+    <testTag joke={jokes[1].joke} author={jokes[1].author} />,
+    <testTag joke={jokes[2].joke} author={jokes[2].author} />,
+  ];
 
   return (
-    <div className=" p-4  flex flex-row justify-end absolute bg-blue-500 w-full">
-      <AiOutlineMenu
-        className="cursor-pointer text-2xl z-[99]"
-        onClick={handleNav}
-      />
-
-      {nav ? (
-        <div className="flex flex-col gap-5 h-screen w-full bg-white  fixed top-0 left-0 justify-center items-center">
-          <LinkTag info={infoIcon.info} icon={infoIcon.icon} />
-          <LinkTag {...infoIcon2} />
-          <LinkTag info={infoIcon.info2} icon={infoIcon.icon2} />
-          <LinkTag info={infoIcon.info3} icon={infoIcon.icon3} />
-          <LinkTag info={infoIcon.info4} icon={infoIcon.icon4} />
-        </div>
-      ) : (
-        ""
-      )}
+    <div>
+      <p>{jokeList}</p>
+      <p>
+        <testTag joke={jokes[0].joke} author={jokes[0].author} />
+      </p>
     </div>
   );
-};
+}
 
 export default Sidenav;
--- src/components/link.jsx
+++ src/components/link.jsx
@@ -1,13 +1,25 @@
-export default function LinkTag({ icon, info }) {
-  return (
-    <div className="w-[80%] flex flex-col items-center justify-center ">
-      <a
-        href=""
-        className="flex rounded-full hover:scale-110 duration-200 ease-in shadow-lg shadow-gray-500 gap-4 p-4 items-center justify-center text-2xl capitalize bg-gray-300 w-full"
-      >
-        <p className="capitalize text-3xl">{icon}</p>
+// function LinkTag({ icon, info }) {
+//   return (
+//     <div className="w-[80%] flex flex-col items-center justify-center ">
+//       <a
+//         href=""
+//         className="flex rounded-full hover:scale-110 duration-200 ease-in shadow-lg shadow-gray-500 gap-4 p-4 items-center justify-center text-2xl capitalize bg-gray-300 w-full"
+//       >
+//         <p className="capitalize text-3xl">{icon}</p>
 
-        <p className="font-bold">{info}</p>
+//         <p className="font-bold">{info}</p>
+//       </a>
+//     </div>
+//   );
+// }
+
+export default function testTag({ joke, author }) {
+  return (
+    <div>
+      <a href="">
+        {joke}
+
+        <p>{author}</p>
       </a>
     </div>
   );
