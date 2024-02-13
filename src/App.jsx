7b76295699453c45d0a22cb3b00ad81f3ba4eaa6
Asuraking913 <israelshedrack913@gmail.com>
2024-02-13 15:09:00 +0100

    React usestate

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,11 @@
+import Joke from "./joke";
+
 import React from "react";
-import Form from "./components/form";
 
 function App() {
-  function handleClick() {
-    console.log("Somethin has been done");
-  }
-
-  function handleChange(e) {
-    console.log(e.target.value);
-  }
-
   return (
-    <div className="h-screen bg-gray-950 flex items-center justify-center">
-      <div className="flex flex-col gap-6">
-        <button
-          className="border-white border-4 p-2 bg-gray-900"
-          onClick={handleClick}
-        >
-          Do something
-        </button>
-        <Form onChange={handleChange} />
-      </div>
+    <div>
+      <Joke />
     </div>
   );
 }
