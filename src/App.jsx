97dc43d5367840e7daeda0d5e77c2c1adcb8dd53
Israel Shedrack <israelshedrack913@gmail.com>
2024-04-23 10:05:23 -0400

    "Halting development: Restart Development in source 2

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,11 @@ import './fonts/font-icons/css/all.min.css'
 
 function App() {
   return (
-    <div className='bg-gray-400 h-screen'>
-      <Nav />
-      <section className='h-[80vh] pb-2 bg-gray-300 rounded-b-3xl'>
-        <div className='bg-orange-500 h-[95%] pt-20 flex flex-col justify-center items-center font-bold size rounded-b-3xl'>
-          <h1 className='text-gray-300 text-[1.2em] w-[50%] text-center'>Expanding your mind reading your books</h1>
-          <div className='flex flex-row '>
-            <input className='bg-gray-200 rounded-full border-none' type="text" /> <button className='bg-gray-200 pr-4 pl-4 ml-[-1.5em] rounded-full'><i className='fa-solid fa-search text-orange-500'></i></button>
-          </div>
-        </div>
-      </section>
+    <div>
+      <div className='color'>
+        <Nav />
       </div>
+    </div>
   )
 }
 
--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 
 function Nav() {
   return (
-    <div className='w-full flex flex-row justify-between pt-4 p-4 px-10 bg-transparent fixed top-0'>
-        <a href="#" className="text-white bg-transparent font1 font-bold text-3xl">Novela</a>
+    <div className='w-full flex flex-row justify-between pt-4 p-4 px-10 fixed top-0 blur1'>
+        <a href="#" className="text-black bg-transparent font2 font-bold text-3xl">RLeague</a>
         <nav>
             <ul className='flex gap-4'>
-                <li><a href="#" className='font-sans font-semibold text-[1.2em]'>About Us</a></li>
-                <li><a href="#" className='font-sans font-semibold text-[1.2em]'>Our Books</a></li>
-                <li><a href="#" className='font-sans font-semibold text-[1.2em]'>Contact Us</a></li>
+                <li><a href="#" className='text-blue-400 font3 font-bold text-[1.2em]'>About Us</a></li>
+                <li><a href="#" className='text-blue-400 font3 font-bold text-[1.2em]'>Our Books</a></li>
+                <li><a href="#" className='text-blue-400 font3 font-bold text-[1.2em]'>Contact Us</a></li>
             </ul>
         </nav>
     </div>
